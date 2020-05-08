e39362c91a6a277f08ec5c487ec0c7d3cbd79fd9
Fayk Chaudhry <faykc123@gmail.com>
2020-05-08 18:36:33 -0400

    Added a re-usable model class

--- model.py
+++ model.py
@@ -1,16 +1,20 @@
 import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-# Testing phase
-tf1 = pickle.load(open("TFIDF/tfidf_vocab.pickle", 'rb'))
+class Model():
+    def __init__(self):
+        # Load the trained model and TFIDF Vector
+        tf1 = pickle.load(open("TFIDF/tfidf_vocab.pickle", 'rb'))
+        self.tfid = TfidfVectorizer(stop_words = "english",vocabulary = tf1.vocabulary_)
+        self.model = pickle.load(open('Models/pa_clf.sav', 'rb'))
+    def transformData(self, newCorpus):
+        return self.tfid.fit_transform(newCorpus)
+    def predict(self, data):
+        return self.model.predict(self.transformData(data))
 
-# Create new tfidfVectorizer with old vocabulary
-tf1_new = TfidfVectorizer(stop_words = "english",vocabulary = tf1.vocabulary_)
-
+"""
+Example use case for this
+myModel = Model()
 new_corpus = ["New York City had alerted Monday that 15 patients between 2 and 15 were hospitalized over the past three weeks with the Covid-related syndrome. Most of them tested positive for coronavirus or had positive antibody tests. Some of the children had persistent fever, toxic shock syndrome, and features similar to Kawasaki disease, the state health advisory said. Kawasaki disease causes inflammation in the walls of the arteries and can limit blood flow to the heart. While it's usually treatable and most children recover without serious problems, it can also be deadly. It mainly affects children under 5."]
-
-X_tf1 = tf1_new.fit_transform(new_corpus)
-
-model = pickle.load(open('Models/pa_clf.sav', 'rb'))
-
-print(model.predict(X_tf1))
+print(myModel.predict(new_corpus))
+"""
