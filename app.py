623cb03fd865f5afdf478ccc04c334ec2223a29d
Fayk Chaudhry <faykc123@gmail.com>
2020-05-09 14:37:49 -0400

    Heroku Updates

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/helloworld')
-def hello():
-    return 'HELLO WORLD BOI!'
+if __name__ == '__main__':
+    app.run()
